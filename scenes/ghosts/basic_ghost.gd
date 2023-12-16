cf63be7ed6b56c0fa5a5afb3b6b88526e730e711
mashumafi <mashumafi@gmail.com>
2023-12-15 23:26:11 -0500

    blending animations

--- scenes/ghosts/basic_ghost.gd
+++ scenes/ghosts/basic_ghost.gd
@@ -9,6 +9,9 @@ var gravity := -9.8
 @export
 var ghost : Node3D
 
+@export
+var blend_tree : AnimationTree
+
 func _process(delta: float) -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * speed
 	velocity = velocity.move_toward(Vector3(direction.x, velocity.y + gravity, direction.y), .4)
@@ -17,5 +20,7 @@ func _process(delta: float) -> void:
 
 	if velocity.length_squared() > 0:
 		ghost.rotation.y = lerp(ghost.rotation.y, atan2(velocity.x, velocity.z), .2)
+
+	blend_tree.set("parameters/move_anim/blend_amount", remap(Vector2(velocity.x, velocity.z).length(), 0, speed, -1, .5))
 
 	move_and_slide()
