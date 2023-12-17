a36a800e061e681ce96104d53e2c916ad4837b90
Matthew Murphy <mashumafi@users.noreply.github.com>
2023-12-17 13:17:50 -0500

    Ghost can die (#54)

--- scenes/possessable/paranormal_activity.gd
+++ scenes/possessable/paranormal_activity.gd
@@ -2,7 +2,7 @@ extends Node
 
 signal contact_made(Possessable) # Called when the ghost can interact with a new object
 signal interacted(Possessable) # Called when the ghost interacts with an object
-signal end_game(bool, String)
+signal end_game(bool, String) # is_win, reason
 
 var investigations : Array[Possessable] = []
 var recently_interacted : Array[Possessable] = []
@@ -47,6 +47,7 @@ func spawn_ghost(node: Node3D):
 	var ghost := preload("res://scenes/ghosts/basic_ghost.tscn").instantiate()
 	var camera := preload("res://scenes/camera.tscn").instantiate()
 	var parent := node.get_parent()
+	HUD.show()
 	ghost.add_child(camera)
 	parent.add_child(ghost)
 	camera.current = true
