908a35d89abf3d4285abc80b3f0540fa3b0fd438
mashumafi <mashumafi@gmail.com>
2023-12-17 13:15:56 -0500

    doc

--- scenes/possessable/paranormal_activity.gd
+++ scenes/possessable/paranormal_activity.gd
@@ -2,7 +2,7 @@ extends Node
 
 signal contact_made(Possessable) # Called when the ghost can interact with a new object
 signal interacted(Possessable) # Called when the ghost interacts with an object
-signal end_game(bool, String) #
+signal end_game(bool, String) # is_win, reason
 
 var investigations : Array[Possessable] = []
 var recently_interacted : Array[Possessable] = []
