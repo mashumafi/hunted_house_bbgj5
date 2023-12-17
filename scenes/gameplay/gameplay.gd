afe756b18821c214316272bd86a999ebde661db7
mashumafi <mashumafi@gmail.com>
2023-12-17 13:11:12 -0500

    Ghost can die

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -4,7 +4,7 @@ func pre_start(params):
 	var cur_scene: Node = get_tree().current_scene
 	print("Scene loaded: ", cur_scene.name, " (", cur_scene.scene_file_path, ")")
 	ParanormalActivity.spawn_ghost($GhostSpawn)
-	
+
 	ParanormalActivity.end_game.connect(process_end_game)
 
 func start():
@@ -12,7 +12,7 @@ func start():
 
 func _process(delta: float) -> void:
 	pass
-	
+
 func _physics_process(delta: float) -> void:
 	var hunters_parent = get_tree().get_nodes_in_group("hunters")[0]
 	if hunters_parent.get_children().size() == 0:
@@ -25,4 +25,4 @@ func process_end_game(win_lose: bool, reason: String):
 		"win_lose": win_lose,
 		"reason": reason
 	}
-	Game.change_scene_to_file("res://scenes/menu/win_lose.tscn", params)
+	Game.change_scene_to_file("res://scenes/menu/win_lose_screen.tscn", params)
