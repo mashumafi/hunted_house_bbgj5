911dd27096125695b5f83a5da49800791dd2fd67
Peyton Williams <peytonwilliams5@gmail.com>
2023-12-17 12:19:00 -0500

    add a basic win-lose screen and a few fixes (#53)

--- scenes/possessable/paranormal_activity.gd
+++ scenes/possessable/paranormal_activity.gd
@@ -2,6 +2,7 @@ extends Node
 
 signal contact_made(Possessable) # Called when the ghost can interact with a new object
 signal interacted(Possessable) # Called when the ghost interacts with an object
+signal end_game(bool, String)
 
 var investigations : Array[Possessable] = []
 var recently_interacted : Array[Possessable] = []
